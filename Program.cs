1107406bc1d020926aac9f2656a932d85e15a27c
Dyp <games123tests@gmail.com>
2023-07-23 22:33:41 +0300

    step 4. Finished Program.cs added other unimportant but essential files

--- Program.cs
+++ Program.cs
@@ -4,10 +4,10 @@ public class SolvingProblems
 {
     public static void Main(string[] args)
     {
-        string[] lines = new string[11] { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"}; // ввод входных данных в массив строк
-        string[] ans = new string[lines.Length];//массив для найденных строк длинной <4
-        int anscnt = 0;//Количество найденных строк длинной <4
-        for (int i = 0; i < lines.Length; i++)//цикл нахождения строк длинной <4
+        string[] lines = new string[11] { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"}; // ввод входных данных в массив строк.
+        string[] ans = new string[lines.Length];//массив для найденных строк длинной <4.
+        int anscnt = 0;//Количество найденных строк длинной <4.
+        for (int i = 0; i < lines.Length; i++)//цикл нахождения строк длинной <4.
         {
             if (lines[i].Length <= 3)
             {
@@ -15,7 +15,7 @@ public class SolvingProblems
                 anscnt++;
             }
         }
-        for (int i = 0; i < ans.Length; i++)//цикл вывода строк длинной <4
+        for (int i = 0; i < ans.Length; i++)//цикл вывода строк длинной <4.
         {
             Console.Write($"{ans[i]} ");
         }
